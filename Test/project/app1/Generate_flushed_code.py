d3f9aa864ae0095de24eaa1d1d1e4b37d028c1b5
tharunkumar453 <vanatharun4@gmail.com>
2026-02-03 02:09:07 +0000

    update python wafer(only primitives)

--- Test/project/app1/Generate_flushed_code.py
+++ Test/project/app1/Generate_flushed_code.py
@@ -1,64 +1,41 @@
+import json
 class TotalCOdeCombiner:
     def combine_originl_tests(self,file,tests):
-        return f'''
+        x=f'''
 {file}  
 {tests}
-'''
-       
+'''     
+        print(x)
+        return x   
 class PythonCombiner:
     @staticmethod
     def combined_file_python(file,test_casess):
         inputs=[tc["input"] for tc in test_casess['cases']]
         ans=[tc["expected"] for tc in test_casess['cases']]
         method=test_casess["method_name"]
-        parameters=test_casess["parameters"]
-        if(parameters=="list_of_integers"):
-            tests=f'''
-def _test_user_code():
+        x=f'''
+def parse(x):
+    if isinstance(x,list):
+        return[parse(v) for v in x]
+    if isinstance(x,dict):
+        return {{ k: parse(v) for k,v in x.items()}}
+    return x
+def driver_code():
     a=Solution()
-    for i,test in enumerate({inputs}):
-       
-        output=a.{method}(test)
-        if(output!={ans}[i]):
-           print(f"wrong answer at test case{{i+1}}")
-           return
-    print("453@")")
-_test_user_code()
-'''
-        elif(parameters=="integer"):
-            tests=f'''
-def _test_user_code():
-    a=Solution()    
-    for i,test in enumerate({inputs}):
-       
-        output=a.{method}(test)
-        if(output!={ans}[i]):
-           print(f"wrong answer at test case{{i+1}}")
-           return
-        print("453@")
-_test_user_code()
-
-
-'''
-        elif(parameters=="string"):
-            tests=f'''
-def _test_user_code():
-    a=Solution()    
-    for i,test in enumerate({inputs}):
-       
-        output=a.{method}(test)
-        e={ans}[i]
-        
-        if(output!=e):
-           print(f"wrong answer at test case{{i+1}}")
-           return   
-
-    print("423@")
-_test_user_code()
-'''
-        else:
-            raise ValueError("Unsupported parameter type")
-        return TotalCOdeCombiner().combine_originl_tests(file,tests)
+    func=getattr(a,'{method}')
+    for  i,(x,y) in enumerate(zip{inputs,ans}):
+        args=parse(x)
+        out=func(*args)
+        exp=parse(y)
+        print(out)
+        print(exp)
+        if(out!=exp):
+            print("error at test case",i+1)
+            return
+    print("Accepted")
+driver_code()
+'''   
+        return TotalCOdeCombiner().combine_originl_tests(file,x)
         
 
 class CppCombiner:
